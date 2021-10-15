4214e4b0b1dcbc9b4ff4ae7361feba4fbbf7c137
Mlendea, Horațiu <Horatiu.Mlendea@ProtonMail.com>
2021-10-15 12:28:45 +0300

    Created new solution

---
+++ TransliterationAPI/Controllers/TransliterationController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TransliterationAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TransliterationController : ControllerBase
+    {
+        [HttpGet]
+        public string Get(
+            [FromQuery] string language,
+            [FromQuery] string text
+        )
+        {
+            return "TEST! You sent: " + text + " in " + language;
+        }
+    }
+}
