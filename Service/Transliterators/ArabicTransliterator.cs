392d86ed49a7db320b813f873288292d34cf01b5
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2022-01-27 16:35:51 +0200

    Improved arabic transliteration

--- Service/Transliterators/ArabicTransliterator.cs
+++ Service/Transliterators/ArabicTransliterator.cs
@@ -130,7 +130,8 @@ namespace TransliterationAPI.Service.Transliterators
 
             fixedText = fixedText.Replace("Ftḥ", "Fatḥ");
             fixedText = fixedText.Replace("ūrfū", "ūrifū");
-            fixedText = fixedText.Replace(" Dī ", " dī ");
+            fixedText = fixedText.Replace(" [Dd]ī[l]* ", " āl-");
+            fixedText = fixedText.Replace("ẖr", "ẖar");
             
             fixedText = Regex.Replace(fixedText, "^Al([ -])*", "al-$1");
             fixedText = Regex.Replace(fixedText, "^Āl([ -])*", "āl-$1");
