3bfcf514f133ab4fd598a9fcef0ccc34fbd22d9f
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2022-02-01 15:22:12 +0200

    Improved inuttitut translit

--- Service/Transliterators/TranslitterationDotComTransliterator.cs
+++ Service/Transliterators/TranslitterationDotComTransliterator.cs
@@ -81,6 +81,7 @@ namespace TransliterationAPI.Service.Transliterators
             }
             else if (language == "iku")
             {
+                fixedText = fixedText.Replace("ᐄ", "i");
                 fixedText = fixedText.Replace("ᐆ", "u");
             }
             else if (language == "kaz")
