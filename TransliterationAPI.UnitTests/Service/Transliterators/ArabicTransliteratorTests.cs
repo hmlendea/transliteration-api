b39328828e50d2ff3932ee6589834371c3d2175d
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2023-05-23 23:26:28 +0300

    Improved arabic translit

--- TransliterationAPI.UnitTests/Service/Transliterators/ArabicTransliteratorTests.cs
+++ TransliterationAPI.UnitTests/Service/Transliterators/ArabicTransliteratorTests.cs
@@ -24,6 +24,7 @@ namespace TransliterationAPI.UnitTests.Service.Transliterators
         [TestCase("دمشق", "Dimašq")]
         [TestCase("روما", "Rūmā")]
         [TestCase("عَسْقَلَان", "ʿAsqalān")]
+        [TestCase("فِلَسْطِينَ", "Filasṭīn")]
         [TestCase("هرتسليا", "Hirtsiliyā")]
         public void GivenATextInArabicScript_WhenTransliteratingIntoLatin_ThenTheCorrectTextIsReturned(
             string arabicText,
