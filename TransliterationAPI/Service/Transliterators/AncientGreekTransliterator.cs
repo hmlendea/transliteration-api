30391db600451c4fba842d48a5a38618aaaf575c
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2023-05-24 12:11:06 +0300

    Improved the Ancient Greek transliteration

--- TransliterationAPI/Service/Transliterators/AncientGreekTransliterator.cs
+++ TransliterationAPI/Service/Transliterators/AncientGreekTransliterator.cs
@@ -5,188 +5,218 @@ namespace TransliterationAPI.Service.Transliterators
 {
     public class AncientGreekTransliterator : IAncientGreekTransliterator
     {
-        Dictionary<char, string> transliterationMap;
-        Dictionary<char, string> transliterationMap2;
+        Dictionary<string, string> transliterationTable;
 
         public AncientGreekTransliterator()
         {
-            transliterationMap = new Dictionary<char, string>
+            transliterationTable = new Dictionary<string, string>
             {
-                { 'α', "a" },
-                { 'ἀ', "a" },
-                { 'β', "v" }, //b
-                { 'γ', "g" },
-                { 'δ', "d" },
-                { 'ε', "e" },
-                { 'ζ', "z" },
-                { 'η', "ē" },
-                { 'θ', "th" },
-                { 'ι', "i" },
-                { 'κ', "k" },
-                { 'λ', "l" },
-                { 'μ', "m" },
-                { 'ν', "n" },
-                { 'ξ', "x" },
-                { 'ο', "o" },
-                { 'π', "p" },
-                { 'ρ', "r" },
-                { 'ς', "s" },
-                { 'σ', "s" },
-                { 'τ', "t" },
-                { 'υ', "u" },
-                { 'φ', "ph" },
-                { 'χ', "ch" },
-                { 'ψ', "ps" },
-                { 'ω', "ō" },
+                { "α", "a" },
+                { "ἀ", "a" },
+                { "β", "v" }, //b
+                { "γ", "g" },
+                { "δ", "d" },
+                { "ε", "e" },
+                { "ζ", "z" },
+                { "η", "ē" },
+                { "θ", "th" },
+                { "ι", "i" },
+                { "κ", "k" },
+                { "λ", "l" },
+                { "μ", "m" },
+                { "ν", "n" },
+                { "ξ", "x" },
+                { "ο", "o" },
+                { "π", "p" },
+                { "ρ", "r" },
+                { "ς", "s" },
+                { "σ", "s" },
+                { "τ", "t" },
+                { "υ", "y" }, // u
+                { "φ", "ph" },
+                { "χ", "ch" },
+                { "ψ", "ps" },
+                { "ω", "ō" },
 
                 // Capital letters
-                { 'Α', "A" },
-                { 'Β', "B" },
-                { 'Γ', "G" },
-                { 'Δ', "D" },
-                { 'Ε', "E" },
-                { 'Ζ', "Z" },
-                { 'Η', "Ē" },
-                { 'Θ', "Th" },
-                { 'Ι', "I" },
-                { 'Κ', "K" },
-                { 'Λ', "L" },
-                { 'Μ', "M" },
-                { 'Ν', "N" },
-                { 'Ξ', "X" },
-                { 'Ο', "O" },
-                { 'Π', "P" },
-                { 'Ρ', "R" },
-                { 'Σ', "S" },
-                { 'Τ', "T" },
-                { 'Υ', "U" },
-                { 'Φ', "Ph" },
-                { 'Χ', "Ch" },
-                { 'Ψ', "Ps" },
-                { 'Ω', "Ō" },
+                { "Α", "A" },
+                { "Ἀ", "A" },
+                { "Ἄ", "Á" },
+                { "ᾈ", "A" },
+                { "Ἁ", "A" },
+                { "Ἅ", "Á" },
+                { "Ά", "Á" },
+                { "Ὰ", "À" },
+                { "Ᾰ", "A" },
+                { "Ᾱ", "Ā" },
+                { "Ἂ", "Â" },
+                { "Ἆ", "Â" },
+                { "Ἃ", "Â" },
+                { "Ἇ", "Â" },
+                { "Α͂", "Â" },
+                { "Αι", "Ā" },
+                { "Άι", "Á̄" },
+                { "Ὰι", "Ā̀" },
+                { "Ἀι", "Āi" },
+                { "Ἄι", "Ái" },
+                { "Ἁι", "Āi" },
+                { "Ἅι", "Ái" },
+                { "Α͂ι", "Â̄" },
+                { "Ἂι", "Âi" },
+                { "Ἆι", "Âi" },
+                { "Ἃι", "Âi" },
+                { "Ἇι", "Âi" },
+                { "Β", "B" },
+                { "Γ", "G" },
+                { "Δ", "D" },
+                { "Ε", "E" },
+                { "Ἐ", "E" },
+                { "Ἔ", "É" },
+                { "Ἒ", "Ê" },
+                { "Ἑ", "E" },
+                { "Ἕ", "É" },
+                { "Ἓ", "Ê" },
+                { "Έ", "É" },
+                { "Ὲ", "È" },
+                { "Ζ", "Z" },
+                { "Η", "Ē" },
+                { "Ἠ", "Ē" },
+                { "Ἢ", "Ê" },
+                { "Ἦ", "Ê" },
+                { "Ἡ", "Hē" }, // Ē
+                { "Ἣ", "Ê" },
+                { "Ἧ", "Ê" },
+                { "Ὴ", "Ḕ" },
+                { "Η͂", "Ê" },
+                { "Ἤ", "Ḗ" }, // Ḗ
+                { "Ἥ", "Ḗ" }, // Ḗ
+                { "Ή", "Ḗ" }, // Ḗ
+                { "Ηι", "Ē̂" },
+                { "Ήι", "Ē̄́" },
+                { "Ὴι", "Ē̄̀" },
+                { "Η͂ι", "Ê̄" },
+                { "Ἠι", "Ēi" },
+                { "Ἤι", "Ēí" },
+                { "Ἢι", "Êi" },
+                { "Ἦι", "Êi" },
+                { "Ἡι", "Ēi" },
+                { "Ἥι", "Ēí" },
+                { "Ἣι", "Êi" },
+                { "Ἧι", "Êi" },
+                { "Θ", "Th" },
+                { "Ι", "I" },
+                { "Ἰ", "I" },
+                { "Ί", "Í" },
+                { "Ϊ", "Ï" },
+                { "Ϊ́", "Ḯ" },
+                { "Κ", "K" },
+                { "Λ", "L" },
+                { "Μ", "M" },
+                { "Ν", "N" },
+                { "Ξ", "X" },
+                { "Ο", "O" },
+                { "Ὀ", "Ó" },
+                { "Ὄ", "Ó" },
+                { "Ό", "Ó" },
+                { "Π", "P" },
+                { "Ρ", "R" },
+                { "Ῥ", "Rh" },
+                { "Σ", "S" },
+                { "Τ", "T" },
+                { "Υ", "U" },
+                { "Υ̓", "Ú" },
+                { "Ύ", "Ú" },
+                { "Ϋ", "Ü" },
+                { "Ϋ́", "Ǘ" },
+                { "Φ", "Ph" },
+                { "Χ", "Ch" },
+                { "Ψ", "Ps" },
+                { "Ω", "Ō" },
+                { "Ώ", "Ṓ" }, // Ṓ
 
-                // Common diacritical marks
-                { 'ά', "á" },
-                { 'έ', "é" },
-                { 'ῆ', "ê" },
-                { 'ή', "ḗ" }, // ḗ
-                { 'ί', "í" },
-                { 'ϊ', "ï" },
-                { 'ΐ', "ḯ" },
-                { 'ό', "ó" },
-                { 'ὐ', "ú" },
-                { 'ύ', "ú" },
-                { 'ϋ', "ü" },
-                { 'ΰ', "ǘ" },
-                { 'ώ', "ṓ" }, // ṓ
-
-                // Capital letters with diacritical marks
-                { 'Ἀ', "A" },
-                { 'Ά', "Á" },
-                { 'Ἐ', "E" },
-                { 'Έ', "É" },
-                { 'Ή', "Ḗ" }, // Ḗ
-                { 'Ἡ', "Hē" },
-                { 'Ἰ', "I" },
-                { 'Ί', "Í" },
-                { 'Ὀ', "Ó" },
-                { 'Ὄ', "Ó" },
-                { 'Ό', "Ó" },
-                { 'Ῥ', "Rh" },
-                { 'Ύ', "Ú" },
-                { 'Ώ', "Ṓ" }, // Ṓ
-
+                // Lowercase letters
+                { "ἄ", "á" },
+                { "ἁ", "a" },
+                { "ἅ", "á" },
+                { "ά", "á" },
+                { "ᾴ", "á̄" },
+                { "ὰ", "à" },
+                { "ᾲ", "ā̀" },
+                { "ᾰ", "a" },
+                { "ᾱ", "ā" },
+                { "ᾳ", "ā" },
+                { "ᾄ", "ái" },
+                { "ᾀ", "āi" },
+                { "ᾅ", "ái" },
+                { "ᾁ", "āi" },
+                { "ἂ", "â" },
+                { "ἆ", "â" },
+                { "ἃ", "â" },
+                { "ἇ", "â" },
+                { "ᾶ", "â" },
+                { "ᾷ", "â̄" },
+                { "ᾂ", "âi" },
+                { "ᾆ", "âi" },
+                { "ᾃ", "âi" },
+                { "ᾇ", "âi" },
+                { "ἐ", "e" },
+                { "ἔ", "é" },
+                { "ἒ", "ê" },
+                { "ἑ", "e" },
+                { "ἕ", "é" },
+                { "ἓ", "ê" },
+                { "έ", "é" },
+                { "ὲ", "è" },
+                { "ἠ", "ē" },
+                { "ἢ", "ê" },
+                { "ἦ", "ê" },
+                { "ἡ", "ē" },
+                { "ἣ", "ê" },
+                { "ἧ", "ê" },
+                { "ῄ", "ē̄́" },
+                { "ὴ", "ḕ" },
+                { "ῂ", "ē̄̀" },
+                { "ῆ", "ê" },
+                { "ῇ", "ê̄" },
+                { "ῃ", "ē̂" },
+                { "ἤ", "ḗ" }, // ḗ
+                { "ἥ", "ḗ" }, // ḗ
+                { "ή", "ḗ" }, // ḗ
+                { "ᾔ", "ēí" },
+                { "ᾒ", "êi" },
+                { "ᾖ", "êi" },
+                { "ᾐ", "ēi" },
+                { "ᾕ", "ēí" },
+                { "ᾓ", "êi" },
+                { "ᾗ", "êi" },
+                { "ᾑ", "ēi" },
+                { "ί", "í" },
+                { "ϊ", "ï" },
+                { "ΐ", "ḯ" },
+                { "ό", "ó" },
+                { "ὐ", "ú" },
+                { "ύ", "ú" },
+                { "ϋ", "ü" },
+                { "ΰ", "ǘ" },
+                { "ώ", "ṓ" }, // ṓ
 
                 // Additional characters
-                { 'ϵ', "e" }, // lunate epsilon
-                { 'ϑ', "th" }, // script theta
-                { 'ϱ', "r" }, // script rho
-                { 'ϖ', "p" }, // lunate pi
-                { 'ϰ', "k" }, // script kappa
-                { 'ϗ', "kai" }, // kai symbol
-            };
-
-            transliterationMap2 = new Dictionary<char, string>
-            {
-                {'ἀ', "a"},
-                {'ἄ', "á"},
-                {'ἁ', "a"},
-                {'ἅ', "á"},
-                {'ά', "á"},
-                {'ᾴ', "á̄"},
-                {'ὰ', "à"},
-                {'ᾲ', "ā̀"},
-                {'ᾰ', "a"},
-                {'ᾱ', "ā"},
-                {'ᾳ', "ā"},
-                {'ᾄ', "ái"},
-                {'ᾀ', "āi"},
-                {'ᾅ', "ái"},
-                {'ᾁ', "āi"},
-                {'ἂ', "â"},
-                {'ἆ', "â"},
-                {'ἃ', "â"},
-                {'ἇ', "â"},
-                {'ᾶ', "â"},
-                {'ᾷ', "â̄"},
-                {'ᾂ', "âi"},
-                {'ᾆ', "âi"},
-                {'ᾃ', "âi"},
-                {'ᾇ', "âi"},
-                {'β', "b"},
-                {'γ', "g"},
-                {'δ', "d"},
-                {'ε', "e"},
-                {'ἐ', "e"},
-                {'ἔ', "é"},
-                {'ἒ', "ê"},
-                {'ἑ', "e"},
-                {'ἕ', "é"},
-                {'ἓ', "ê"},
-                {'έ', "é"},
-                {'ὲ', "è"},
-                {'ζ', "z"},
-                {'η', "ē"},
-                {'ἠ', "ē"},
-                {'ἤ', "ḗ"}, // ḗ
-                {'ἢ', "ê"},
-                {'ἦ', "ê"},
-                {'ἡ', "ē"},
-                {'ἥ', "ḗ"}, // ḗ
-                {'ἣ', "ê"},
-                {'ἧ', "ê"},
-                {'ή', "ḗ"}, // ḗ
-                {'ῄ', "ē̄́"},
-                {'ὴ', "ḕ"},
-                {'ῂ', "ē̄̀"},
-                {'ῆ', "ê"},
-                {'ῇ', "ê̄"},
-                {'ῃ', "ē̂"},
-                {'ᾔ', "ēí"},
-                {'ᾒ', "êi"},
-                {'ᾖ', "êi"},
-                {'ᾐ', "ēi"},
-                {'ᾕ', "ēí"},
-                {'ᾓ', "êi"},
-                {'ᾗ', "êi"},
-                {'ᾑ', "ēi"},
+                { "ϵ", "e" }, // lunate epsilon
+                { "ϑ", "th" }, // script theta
+                { "ϱ", "r" }, // script rho
+                { "ϖ", "p" }, // lunate pi
+                { "ϰ", "k" }, // script kappa
+                { "ϗ", "kai" }, // kai symbol
             };
         }
 
         public string Transliterate(string text)
         {
-            string transliteratedText = string.Empty;
+            string transliteratedText = text;
 
-            foreach (char character in text)
+            foreach (string marathiCharacter in transliterationTable.Keys)
             {
-                if (transliterationMap.ContainsKey(character))
-                {
-                    transliteratedText += transliterationMap[character];
-                }
-                else
-                {
-                    transliteratedText += character;
-                }
+                transliteratedText = transliteratedText.Replace(marathiCharacter, transliterationTable[marathiCharacter]);
             }
 
             transliteratedText = ApplyFixes(transliteratedText);
@@ -199,21 +229,16 @@ namespace TransliterationAPI.Service.Transliterators
             string fixedText = text;
 
             fixedText = Regex.Replace(fixedText, "Ach", "Akh");
-            fixedText = Regex.Replace(fixedText, "Buz", "Byz");
-            fixedText = Regex.Replace(fixedText, "Ku", "Ky");
             fixedText = Regex.Replace(fixedText, "Kú", "Ký");
-            fixedText = Regex.Replace(fixedText, "Mu([gk])", "My$1");
 
+            fixedText = Regex.Replace(fixedText, "([aeio])y", "$1u");
             fixedText = Regex.Replace(fixedText, "([hk])ê([^n])", "$1ē$2");
             fixedText = Regex.Replace(fixedText, "che([ií])", "khe$1");
-            fixedText = Regex.Replace(fixedText, "gup", "gyp");
-            fixedText = Regex.Replace(fixedText, "lu([mn])", "ly$1");
             fixedText = Regex.Replace(fixedText, "óvo", "óbo");
-            fixedText = Regex.Replace(fixedText, "rut", "ryt");
             fixedText = Regex.Replace(fixedText, "v([úý])", "bý");
             fixedText = Regex.Replace(fixedText, "vai", "bai");
             fixedText = Regex.Replace(fixedText, "vos", "bos");
-            fixedText = Regex.Replace(fixedText, "vul", "byl");
+            fixedText = Regex.Replace(fixedText, "vy", "by");
 
             return fixedText;
         }
