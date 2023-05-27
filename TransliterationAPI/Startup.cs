6d76d64f8adb21c5b34261a18f176aa308a8cea7
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2023-05-27 15:20:28 +0300

    refactoring: Create empty cache store if missing

--- TransliterationAPI/Startup.cs
+++ TransliterationAPI/Startup.cs
@@ -1,9 +1,14 @@
+using System;
+using System.IO;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+
+using TransliterationAPI.Configuration;
 
 namespace TransliterationAPI
 {
@@ -15,11 +20,11 @@ namespace TransliterationAPI
         }
 
         public IConfiguration Configuration { get; }
+        public IServiceProvider ServiceProvider { get; private set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddConfigurations(Configuration);
             services.AddCustomServices();
@@ -27,6 +32,8 @@ namespace TransliterationAPI
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "TransliterationAPI", Version = "v1" });
             });
+
+            ServiceProvider = services.BuildServiceProvider();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +56,24 @@ namespace TransliterationAPI
             {
                 endpoints.MapControllers();
             });
+
+            CacheSettings cacheSettings = ServiceProvider.GetService<CacheSettings>();
+            if (!File.Exists(cacheSettings.StoreLocation))
+            {
+                CreateCacheStore(cacheSettings.StoreLocation);
+            }
+        }
+
+        public static void CreateCacheStore(string storeLocation)
+        {
+            string storeDirectoryLocation = Path.GetDirectoryName(storeLocation);
+
+            if (!string.IsNullOrWhiteSpace(storeDirectoryLocation))
+            {
+                Directory.CreateDirectory(storeDirectoryLocation);
+            }
+
+            File.WriteAllText(storeLocation, "[]");
         }
     }
 }
